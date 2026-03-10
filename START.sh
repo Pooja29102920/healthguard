924e1f9ac26ca2f23e772f54b32cbee5a762f8b9
Kanipakam Poojitha <kanipakampoojitha2007@gmail.com>
2026-03-10 10:52:54 +0530

    Deletion and updation

--- START.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "HealthGuard v10 - Starting..."
-cd "$(dirname "$0")/backend"
-echo "Server starting at http://localhost:8000"
-echo "Open your browser to http://localhost:8000"
-echo "Press Ctrl+C to stop"
-python3 server.py
