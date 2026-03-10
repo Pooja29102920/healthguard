3083beb0fa7c597e345ede85da4574d8394b063f
Kanipakam Poojitha <kanipakampoojitha2007@gmail.com>
2026-03-10 12:50:55 +0530

    Add files via upload

---
+++ frontend/sw.js
@@ -0,0 +1,41 @@
+/* HealthGuard Service Worker v6 – Full offline cache */
+const CACHE = 'healthguard-v6';
+const ASSETS = [
+  './',
+  './index.html',
+  './patient.html',
+  './caregiver.html',
+  './app.css',
+  './manifest.json',
+  './js/storage.js',
+  './js/notifications.js',
+  './js/location.js',
+  './icons/icon-72.png',
+  './icons/icon-96.png',
+  './icons/icon-128.png',
+  './icons/icon-144.png',
+  './icons/icon-152.png',
+  './icons/icon-192.png',
+  './icons/icon-384.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
